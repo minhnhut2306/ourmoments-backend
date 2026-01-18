9e67bbbca26f84cf84c8d448b13f0bebace2dce0
minhnhut2306 <129873898+minhnhut2306@users.noreply.github.com>
2026-01-18 22:13:22 +0700

    upload

--- src/config/cors.js
+++ src/config/cors.js
@@ -5,7 +5,7 @@ module.exports = cors({
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
-    "https://ourmoments-edu.vercel.app"
+    "https://ourmoments-backend.vercel.app/"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
